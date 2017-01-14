61e6d7ad72044596c23a0fdfb0dfef1869072ab6
Nate Northway <northwayn@gmail.com>
2017-01-14 17:11:15 -0600

    Scope of 'isSafari'
    The variable scope of the safari flag was wrong.

--- wp-content/themes/gam/assets/js/scripts.js
+++ wp-content/themes/gam/assets/js/scripts.js
@@ -1,9 +1,8 @@
+var isSafari = false;
 $(document).ready(function() {
   $("#menu-toggle").on('click', function() {
     navToggle();
   })
-
-  var isSafari = false;
 
   if (navigator.userAgent.indexOf('Safari') != -1 && navigator.userAgent.indexOf('Chrome') == -1) {
     isSafari = true;
